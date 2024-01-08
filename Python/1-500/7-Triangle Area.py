99572c4e9828eb2033b4183862dc3d26ba05d338
code <softwaredev545@gmail.com>
2024-01-09 00:40:13 +0530

    9 jan
    Signed-off-by: code <softwaredev545@gmail.com>

--- Python/1-500/7-Triangle Area.py
+++ Python/1-500/7-Triangle Area.py
@@ -1,5 +1,27 @@
 # WAP to find the area of triangle.
 try:
-    n=int(input('Please select your choice. \n1.Right Angle triangle \n2.Isoceles Triangle \n3.Equilateral triangle \n4.Scalene Triangle end= '))
+    print('Enter Your Choice.')
+    print('Press 1 for Right angle Triangle.')
+    print('Press 2 for Equilateral Triangle.')
+    print('Press 3 for Isosceles Triangle.')
+    print('Press 4 for Scalene Triangle.')
+    n = int(input())
 except:
-    print('You have entered wrong choice ') 
+    print('You have entered wrong choice ')
+    exit()
+area = 0.0
+if 1 == n:
+    b = float(input('Please Enter the base of triangle: '))
+    h = float(input('Please Enter the Height of triangle: '))
+    area = 0.5 * b * h
+elif 2 == n:
+    x = float(input('Enter the side of triangle: '))
+    area = (1.732/4) * x**2
+elif 3 == n:
+    a = float(input('Enter the length of equal sides: '))
+    b = float(input('Enter the length of non-equal side: '))
+
+
+
+
+
